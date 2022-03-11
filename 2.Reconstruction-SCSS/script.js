e373ad74c3cb985772b8a7468a9a35a02a2ae9a5
Claire_M <clairerodriguez79@yahoo.fr>
2022-03-11 21:08:13 +0100

    Idem avec syntaxe async et await

--- 2.Reconstruction-SCSS/script.js
+++ 2.Reconstruction-SCSS/script.js
@@ -1,18 +1,45 @@
-function testFetch() {
-    fetch("./data/photographers.json")
-        .then(function(res) {
-            if (res.ok) {
-                return res.json();
-            }
-        })
-        .then(function(value) {
+// Fetch classique
+// function testFetch() {
+//     fetch("./data/photographers.json")
+//         .then(function(response) {
+//             if (response.ok) {
+//                 return response.json();
+//             }
+//         })
+//         .then(function(data) {
 
-            return console.log(value);
-        })
-        .catch(function(err) {
-            // Une erreur est survenue
-            console.log("Pb vec fetch");
-        });
+//             return console.log(data);
+//         })
+//         .catch(function(err) {
+//             // Une erreur est survenue
+//             console.log("Pb vec fetch");
+//         });
+// }
+
+// console.log(testFetch());
+
+// Fetch avec async et await
+
+async function printData() {
+    try {
+        const data = await fetch("./data/photographers.json");
+        // console.log("data", data);
+        let response = await data.json()
+            // console.log("La réponse", response);
+        return response;
+    } catch (erreur) {
+        console.log(erreur);
+    }
 }
+// printData().then(response => console.log(response));
+//Ou
+printData().then(function(response) {
+    console.log(response);
+});
 
-console.log(testFetch());
+//ça ça fonctionne aussi:
+// async function getData() {
+//     let response = await fetch("./data/photographers.json");
+//     let data = await response.json()
+//     return data;
+// }
