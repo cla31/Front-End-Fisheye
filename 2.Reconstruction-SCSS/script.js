33877cffc836c6d90dc2282cb72686872df69983
Claire_M <clairerodriguez79@yahoo.fr>
2022-03-15 15:23:10 +0100

    Affichage de tous les photographes et leurs datas ds la page d'accueil et découpage du code ds le script.js en plusieurs fonctions: fonction pour récupérer les données via le fetch, fonction template, fonction qui gère l'affichage du template, fonction qui récupère les données et les affiche.

--- 2.Reconstruction-SCSS/script.js
+++ 2.Reconstruction-SCSS/script.js
@@ -11,7 +11,28 @@ async function getDatas(pathJson) {
     console.log("réponse de backFetch", backFetch);
     return backFetch;
 }
+//fonction template
+function templatePhotographers(photographersTemplate) {
+    return `
+    <div class="photographer">
+    <a href="photographer.html">
+        <div class="card">
+            <div class="container-img">
+                <img class="container-img__img" src="assets/photographers/Photographers ID Photos/${photographersTemplate.portrait}" />
+            </div>
+            <h2 class="card__photographer-name">${photographersTemplate.name}</h2>
+        </div>
+    </a>
+    <p>${photographersTemplate.city}</p>
+    <p>${photographersTemplate.tagline}</p>
+    <p>${photographersTemplate.price}</p>
+</div>`
+}
+//Fonction qui gère l'affichage du template
+async function displayHTML(id, boxDataJson) {
+    document.getElementById(id).innerHTML = ` ${boxDataJson.map(templatePhotographers).join(' ')}`
 
+}
 
 
 // Fonction qui récupère les données et les affiche
@@ -19,20 +40,27 @@ async function renderDatas(pathJson) {
     const jsonDatas = await getDatas(pathJson);
     console.log("Réponse de renderDatas", jsonDatas);
     console.log(jsonDatas.photographers[0].name);
-    document.getElementById("row").innerHTML = `
-    <div class="photographer">
-    <a href="photographer.html">
-        <div class="card">
-            <div class="container-img">
-                <img class="container-img__img" src="assets/photographers/Photographers ID Photos/${jsonDatas.photographers[0].portrait}" />
-            </div>
-            <h2 class="card__photographer-name">${jsonDatas.photographers[0].name}</h2>
-        </div>
-    </a>
-    <p>${jsonDatas.photographers[0].city}</p>
-    <p>${jsonDatas.photographers[0].tagline}</p>
-    <p>${jsonDatas.photographers[0].price}</p>
-</div>`
+
+    // function template(photographersTemplate) {
+    //     return `
+    //     <div class="photographer">
+    //     <a href="photographer.html">
+    //         <div class="card">
+    //             <div class="container-img">
+    //                 <img class="container-img__img" src="assets/photographers/Photographers ID Photos/${photographersTemplate.portrait}" />
+    //             </div>
+    //             <h2 class="card__photographer-name">${photographersTemplate.name}</h2>
+    //         </div>
+    //     </a>
+    //     <p>${photographersTemplate.city}</p>
+    //     <p>${photographersTemplate.tagline}</p>
+    //     <p>${photographersTemplate.price}</p>
+    // </div>`
+
+    // }
+    // document.getElementById("row").innerHTML = ` ${jsonDatas.photographers.map(template).join(' ')}`
+    displayHTML("row", jsonDatas.photographers);
+
 
 }
 renderDatas(path);
