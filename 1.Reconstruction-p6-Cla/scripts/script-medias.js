1bbac58aab62359433433513ed517a1303dd3613
Claire_M <clairerodriguez79@yahoo.fr>
2022-03-18 14:09:20 +0100

    Commentaires de ce qui ne me sert pas mais à garder au cas où (adresses sites aide)

--- 1.Reconstruction-p6-Cla/scripts/script-medias.js
+++ 1.Reconstruction-p6-Cla/scripts/script-medias.js
@@ -4,42 +4,57 @@ console.log(queryString_url_id);
 const id_number = queryString_url_id.slice(1);
 console.log(id_number);
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-const result = words.filter(word => word.length > 6);
+// const result = words.filter(word => word.length > 6);
 
-console.log(result);
-// expected output: Array ["exuberant", "destruction", "present"]`
-//https://stackoverflow.com/questions/7364150/find-object-by-id-in-an-array-of-javascript-objects
-myArray = [{ 'id': '73', 'foo': 'bar' }, { 'id': '45', 'foo': 'bar' }]
-test1 = myArray.find(x => x.id === '45').foo; //renvoie bar parce que c'est la valeur de la clé foo dt id=45
-console.log("test 1", test1);
-test2 = myArray.filter(x => x.id === '45');
-console.log("test 2", test2);
-
-
+// console.log(result);
+// // expected output: Array ["exuberant", "destruction", "present"]`
+// //https://stackoverflow.com/questions/7364150/find-object-by-id-in-an-array-of-javascript-objects
+// myArray = [{ 'id': '73', 'foo': 'bar' }, { 'id': '45', 'foo': 'bar' }]
+// test1 = myArray.find(x => x.id === '45').foo; //renvoie bar parce que c'est la valeur de la clé foo dt id=45
+// console.log("test 1", test1);
+// test2 = myArray.filter(x => x.id === '45');
+// console.log("test 2", test2);
 
 // expected output: 12
 // chemin du json ds une variable
 const pathJsonProject = "./data/photographers.json";
 // getDatas(pathJsonProject);
-async function datas_with_id(pathJson) {
-    try {
-        const jsonDatas = await getDatas(pathJson);
-        console.log("1 élément ds la fonction", jsonDatas.photographers);
-        const select_id = jsonDatas.photographers.filter(element => element.id == id_number);
-        console.log("en selectionnant l'id", select_id);
-        for (let i in jsonDatas.photographers) {
-            console.log(jsonDatas.photographers[i]["id"]);
-        }
 
+//fonction template pour le header du fichier photographers.html
+
+
+//Fonction qui récupère les données en fonction de l'id
+async function datas_with_id(pathJson, getId) {
+    try {
+        const jsonDatas = await getDatas(pathJson, getId);
+        console.log("1 élément ds la fonction", jsonDatas.photographers);
+        const select_id = jsonDatas.photographers.filter(element => element.id == getId);
+        console.log("en selectionnant l'id", select_id);
+        return select_id;
+        // console.log("en selectionnant l'id et le name", select_id.name);
+        // for (let i in jsonDatas.photographers) {
+        //     console.log(jsonDatas.photographers[i]["id"]);
+        // }   
     } catch (erreur) {
         console.log(erreur);
     }
 }
-datas_with_id(pathJsonProject);
-// console.log("La REP", response);
 
+const datas_medias = datas_with_id(pathJsonProject, id_number);
+
+
+//Fonction qui gère l'affichage du template
+async function displayHTML(id, boxDatasMedia) {
+    try {
+        document.getElementById(id).innerHTML = `<h1>${boxDatasMedia.name}</h1>`
+    } catch (erreur) {
+        console.log(erreur);
+    }
+}
+
+displayHTML("photograph-header", datas_medias);
 
 //fonction template pour médias
 // function templateMedias(photographersMedias) {
