f459514247c2c6a06b472896b179c01630ef1bc9
Claire_M <clairerodriguez79@yahoo.fr>
2022-03-30 21:52:01 +0200

    Nettoyage commentaires et console.log ds la fonction ObjectImages() et création de la fonction ObjectVideos;

--- 1.Reconstruction-p6-Cla/scripts/medias/appMedias.js
+++ 1.Reconstruction-p6-Cla/scripts/medias/appMedias.js
@@ -1,18 +1,12 @@
-//affichage de la page photographe.html en procédural
 //Récupération de l'id dans la chaîne de requête de l'url
 const queryString_url_id = window.location.search;
-// console.log(queryString_url_id);
 const id_number = queryString_url_id.slice(1);
-// console.log(id_number);
 
 async function datas_with_id(pathJson, getId) {
     try {
         const jsonDatas = await getDatas(pathJson, getId);
-        // console.log(jsonDatas);
         const select_id_photogaphers = jsonDatas.photographers.find(element => element.id == getId);
-        // console.log("select_id_photographers", select_id_photogaphers)
         const select_id_medias = jsonDatas.media.filter(element => element.photographerId == getId);
-        // console.log("medias", medias)
         return [select_id_photogaphers, select_id_medias];
     } catch (erreur) {
         console.log(erreur);
@@ -21,8 +15,6 @@ async function datas_with_id(pathJson, getId) {
 
 async function displayHTMLheader(id, boxDatas) {
     try {
-        // console.log("Affichage de box data ds display", boxDatas);
-        // console.log("Affichage de box data ds display", boxDatas.name);
         photograph = new Photographer(boxDatas.id, boxDatas.portrait, boxDatas.name, boxDatas.city, boxDatas.tagline, boxDatas.price, boxDatas.country);
         document.getElementById(id).innerHTML = ` ${photograph.templateHeaderPhotographerHTML()}`;
     } catch (erreur) {
@@ -31,47 +23,47 @@ async function displayHTMLheader(id, boxDatas) {
 }
 
 async function ObjectImages(boxDatas) {
-    //********* Les objets image
     var images = boxDatas.filter(function(img) {
         return img.image;
     });
-    // console.log("images", images)
     photos = images.map(function instance(objectPhoto) {
         return new Image(objectPhoto.id, objectPhoto.photographerId, objectPhoto.title, objectPhoto.image, objectPhoto.likes, objectPhoto.date, objectPhoto.price);
     });
-    // console.log("photos", photos);
     const arrayTemplatePhotos = photos.map(img => { return img.displayImage() });
     return arrayTemplatePhotos;
 }
 
+async function ObjectVideos(boxDatas) {
+    //********* Les objets video
+    var videos = boxDatas.filter(function(film) {
+        return film.video;
+    });
+    // console.log("videos", videos);
+    var films = videos.map(function instance(objectVideo) {
+        return new Video(objectVideo.id, objectVideo.photographerId, objectVideo.title, objectVideo.video, objectVideo.likes, objectVideo.date, objectVideo.price);
+    });
+    // console.log("films", films);
+    const arrayTemplateVideo = films.map(movie => { return movie.displayVideo() });
+    // document.getElementById(id).innerHTML = `${arrayTemplateVideo.join('')}`;
+    return arrayTemplateVideo;
+
+}
 async function displayHTMLmedias(id, boxDatas) {
     try {
-        // console.log("displayHTMLmedias", boxDatas[8].video);
         arrayGalleryPhotosVideos = [];
-        //********* Les objets image
-        // var images = boxDatas.filter(function(img) {
-        //     return img.image;
-        // });
-        // // console.log("images", images)
-        // photos = images.map(function instance(objectPhoto) {
-        //     return new Image(objectPhoto.id, objectPhoto.photographerId, objectPhoto.title, objectPhoto.image, objectPhoto.likes, objectPhoto.date, objectPhoto.price);
-        // });
-        // // console.log("photos", photos);
-        // const arrayTemplatePhotos = photos.map(img => { return img.displayImage() });
-        // // document.getElementById(id).innerHTML = `${arrayTemplatePhotos.join('')}`;
         arrayTemplatePhotos = await ObjectImages(boxDatas);
-        console.log("Retour fonction objet photo", arrayTemplatePhotos)
-            //********* Les objets video
-        var videos = boxDatas.filter(function(film) {
-            return film.video;
-        });
-        // console.log("videos", videos);
-        var films = videos.map(function instance(objectVideo) {
-            return new Video(objectVideo.id, objectVideo.photographerId, objectVideo.title, objectVideo.video, objectVideo.likes, objectVideo.date, objectVideo.price);
-        });
-        // console.log("films", films);
-        const arrayTemplateVideo = films.map(movie => { return movie.displayVideo() });
-        // document.getElementById(id).innerHTML = `${arrayTemplateVideo.join('')}`;
+        arrayTemplateVideo = await ObjectVideos(boxDatas);
+        //********* Les objets video
+        // var videos = boxDatas.filter(function(film) {
+        //     return film.video;
+        // });
+        // // console.log("videos", videos);
+        // var films = videos.map(function instance(objectVideo) {
+        //     return new Video(objectVideo.id, objectVideo.photographerId, objectVideo.title, objectVideo.video, objectVideo.likes, objectVideo.date, objectVideo.price);
+        // });
+        // // console.log("films", films);
+        // const arrayTemplateVideo = films.map(movie => { return movie.displayVideo() });
+        // // document.getElementById(id).innerHTML = `${arrayTemplateVideo.join('')}`;
 
         //Les images et vidéos:
         arrayGalleryPhotosVideos = arrayTemplatePhotos.concat(arrayTemplateVideo);
