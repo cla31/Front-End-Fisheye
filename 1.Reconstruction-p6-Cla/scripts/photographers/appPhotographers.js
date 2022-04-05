4ad51e5d6e20053011e0719acdf3662187884acd
Claire_M <clairerodriguez79@yahoo.fr>
2022-04-05 13:59:09 +0200

    Retrait commentaires
    Ds le fichier appPhotographers.js

--- 1.Reconstruction-p6-Cla/scripts/photographers/appPhotographers.js
+++ 1.Reconstruction-p6-Cla/scripts/photographers/appPhotographers.js
@@ -1,31 +1,3 @@
-//Fonction qui gère l'affichage du template
-
-// async function displayHTML(id, boxDataJson) {
-//     try {
-//         console.log("Affichage de box data ds display", boxDataJson);
-//         photographers = boxDataJson.map(function instance(objectPhotographer) {
-//             return new Photographer(objectPhotographer.id, objectPhotographer.portrait, objectPhotographer.name, objectPhotographer.city, objectPhotographer.tagline, objectPhotographer.price);
-//         });
-//         document.getElementById(id).innerHTML = ` ${photographers.map( photograph =>  {return photograph.templateIndexHTML()
-//         }).join('')}`;
-
-//     } catch (erreur) {
-//         console.log(erreur);
-//     }
-// }
-
-
-// async function orchestratorDatas(pathJson) {
-//     try {
-//         const jsonDatas = await getDatas(pathJson);
-//         displayHTML("photographers", jsonDatas.photographers);
-
-//     } catch (erreur) {
-//         console.log(erreur);
-//     }
-// }
-// orchestratorDatas(pathJsonProject);
-
 async function instancePhotographers(boxDataJson) {
     try {
         const dataPhotographers = boxDataJson.photographers;
