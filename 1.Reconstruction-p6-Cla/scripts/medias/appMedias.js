f1b5c05ab9d1f9adb8886def17fae799f175e5f1
Claire_M <clairerodriguez79@yahoo.fr>
2022-04-07 10:53:08 +0200

    tableau des tris fonctionnels
    Qqq retouches css à faire..

--- 1.Reconstruction-p6-Cla/scripts/medias/appMedias.js
+++ 1.Reconstruction-p6-Cla/scripts/medias/appMedias.js
@@ -12,7 +12,7 @@ async function datas_with_id(pathJson, getId) {
         console.log(erreur);
     }
 }
-
+//Partie Header
 async function displayHTMLheader(id, boxDatas) {
     try {
         photograph = new Photographer(boxDatas);
@@ -21,7 +21,17 @@ async function displayHTMLheader(id, boxDatas) {
         console.log(erreur);
     }
 }
+async function printHeader() {
+    try {
+        const datas_medias = await datas_with_id(pathJsonProject, id_number);
+        const datasPhotographers = datas_medias[0];
+        displayHTMLheader("photograph-header", datasPhotographers);
+    } catch (erreur) {
+        console.log(erreur);
+    }
+}
 
+//Partie Main
 async function filterElements(boxDatas, elements) {
     try {
         if (elements == "image") {
@@ -39,16 +49,6 @@ async function filterElements(boxDatas, elements) {
     }
 }
 
-
-async function arrayTemplatesObjects(arrayObjectsElements) {
-    try {
-        arrayTemplates = arrayObjectsElements.map(element => { return element.display() });
-        return arrayTemplates;
-    } catch (erreur) {
-        console.log(erreur);
-    }
-}
-
 async function concatArray(myArray1, myArray2) {
     try {
         arrayConcat = myArray1.concat(myArray2);
@@ -58,25 +58,6 @@ async function concatArray(myArray1, myArray2) {
     }
 }
 
-
-async function displayHTMLmedias(id, boxTemplates) {
-    try {
-        document.getElementById(id).innerHTML = `${boxTemplates.join('')}`;
-    } catch (erreur) {
-        console.log(erreur);
-    }
-}
-
-async function printHeader() {
-    try {
-        const datas_medias = await datas_with_id(pathJsonProject, id_number);
-        const datasPhotographers = datas_medias[0];
-        displayHTMLheader("photograph-header", datasPhotographers);
-
-    } catch (erreur) {
-        console.log(erreur);
-    }
-}
 async function arrayObjectMedias(datasImages, datasVideos) {
     try {
         const objImages = await arrayObject(datasImages, ImageMedia);
@@ -89,7 +70,7 @@ async function arrayObjectMedias(datasImages, datasVideos) {
         console.log(erreur);
     }
 }
-async function arrayElements() {
+async function arrayElementsRaw() {
     try {
         const datas_medias = await datas_with_id(pathJsonProject, id_number);
         const datasImages = await filterElements(datas_medias[1], "image");
@@ -103,10 +84,25 @@ async function arrayElements() {
         console.log(erreur);
     }
 }
+async function arrayTemplatesObjects(arrayObjectsElements) {
+    try {
+        arrayTemplates = arrayObjectsElements.map(element => { return element.display() });
+        return arrayTemplates;
+    } catch (erreur) {
+        console.log(erreur);
+    }
+}
+async function displayHTMLmedias(id, boxTemplates) {
+    try {
+        document.getElementById(id).innerHTML = `${boxTemplates.join('')}`;
+    } catch (erreur) {
+        console.log(erreur);
+    }
+}
 
 async function printGallery() {
     try {
-        const arrayMedias = await arrayElements();
+        const arrayMedias = await arrayElementsRaw();
         const arrayTemplate = await arrayTemplatesObjects(arrayMedias);
         // console.log("tableau des templates", arrayTemplate);
         displayHTMLmedias("medias", arrayTemplate);
@@ -115,15 +111,111 @@ async function printGallery() {
         console.log(erreur);
     }
 }
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//Tableau de tri
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Par popularité
+async function sortArrayLikes() {
+    const arrayMedias = await arrayElementsRaw();
+    // console.log("Array au clic", arrayMedias);
+    const result = arrayMedias.sort((a, b) => {
+        if (a.likes > b.likes) {
+            return -1;
+        } else if (a.likes < b.likes) {
+            return 1;
+        } else {
+            return 0;
+        };
+    });
+    console.log("Result au clic", result);
+    return result;
+}
+async function displaySortLikes() {
+    const arrayLikes = await sortArrayLikes();
+    // console.log("result array", arrayLikes);
+    const arrayTemplate = await arrayTemplatesObjects(arrayLikes);
+    displayHTMLmedias("medias", arrayTemplate);
+}
+//Par titre   ********************************************
+async function SortArrayTitles() {
+    const arrayMedias = await arrayElementsRaw();
+    const SortByName = arrayMedias.sort((a, b) =>
+        a.title.localeCompare(b.title));
+    return SortByName;
+
+}
+async function displaySortTitles() {
+    const arrayTitles = await SortArrayTitles();
+    const arrayTemplate = await arrayTemplatesObjects(arrayTitles);
+    displayHTMLmedias("medias", arrayTemplate);
+}
+//Par date   ********************************************
+
+
+async function SortArrayDates() {
+    const arrayMedias = await arrayElementsRaw();
+    const SortByDates = arrayMedias.sort(function(a, b) {
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return new Date(b.date) - new Date(a.date);
+    });
+    return SortByDates;
+}
+async function displaySortDates() {
+    const arrayDates = await SortArrayDates();
+    console.log("Array of dates", arrayDates);
+    const arrayTemplate = await arrayTemplatesObjects(arrayDates);
+    displayHTMLmedias("medias", arrayTemplate);
+}
+//Gestion du tri
+async function sorting() {
+    const sort = document.getElementById('toSort');
+    sort.addEventListener('click', function() {
+        console.log("C'est cliqué!");
+        var text = sort.options[sort.selectedIndex].text;
+        console.log(text);
+        switch (text) {
+            case 'Popularité':
+                // console.log("T'as cliqué sur Popularité!!");
+                displaySortLikes();
+                break;
+            case 'Date':
+                // console.log("T'as cliqué sur Date!!");
+                displaySortDates();
+                break;
+            case 'Titre':
+                // console.log("T'as cliqué sur Titre!!");
+                displaySortTitles();
+                break;
+            default:
+                null;
+        }
+    });
+    // const sortDate = document.getElementsByClassName('date');
+    // sortDate.addEventListener('click', function() {
+    //     console.log("C'est cliqué!");
+    // });
+}
+
+// async function sortingDate() {
+//     const sortDate = document.getElementsByClassName('date');
+//     sortDate.addEventListener('click', function() {
+//         console.log("C'est cliqué!");
+//     });
+// }
 
 const orchestratorPhotographerHTML = async() => {
     try {
         printHeader();
         printGallery();
+        sorting();
+        // sortingDate();
     } catch (erreur) {
         console.log(erreur);
     }
 }
+
+
 
 orchestratorPhotographerHTML();
 
