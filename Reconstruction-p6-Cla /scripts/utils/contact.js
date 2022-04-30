04d3b305cf21eb07662b0bfad5d749e81ca933e0
Claire_M <clairerodriguez79@yahoo.fr>
2022-05-01 00:26:15 +0200

    Mecanique du formulaire contact ok:reste check message?
    et css aussi.

--- Reconstruction-p6-Cla /scripts/utils/contact.js
+++ Reconstruction-p6-Cla /scripts/utils/contact.js
@@ -2,20 +2,20 @@ function contact(photogapherName) {
     const form = document.getElementById("contact");
     const modale = document.getElementById("form-contact");
 
+    //Pour afficher le formulaire
     function displayForm(photogapherName, modale) {
         modale.style.display = "block";
-
         modale.innerHTML =
             ` <p>Contactez-moi <br> ${photogapherName}</p>
-            <form>        
+            <form id="form">        
             <div class="first-formData">
                 <label for="prenom">Prénom</label><br>
-                <input type="text" name="prenom" id="prenom"/>
+                <input type="text" name="prenom" id="first"/>
                 <span></span>
             </div>       
             <div class="last-formData">
                 <label for="nom">Nom</label><br>
-                <input type="text" name="nom" id="nom"/>
+                <input type="text" name="nom" id="last"/>
                 <span></span>
             </div>       
             <div class="email-formData">
@@ -25,20 +25,140 @@ function contact(photogapherName) {
             </div>        
             <p>
                 <label for="message">Message</label><br />
-                <textarea name="ameliorer" id="ameliorer"></textarea>
+                <textarea name="ameliorer" id="improve"></textarea>
                 <span></span>
             </p>
             <input type="submit" value="Envoyer">
             </form>
             <div class="close-contact" id="close-form">Close</div>    
         `;
-        document.getElementById("close-form").addEventListener("click", function() {
-            modale.style.display = "none";
+
+    }
+    //soumission du formulaire
+    function submitForm() {
+        //Selection ds le dom du formulaire
+        const onlyForm = document.querySelector("#form");
+        // console.log("Juste le formulaire", onlyForm);
+        //fermeture du formulaire
+        function closeForm() {
+            document.getElementById("close-form").addEventListener("click", function() {
+                modale.style.display = "none";
+            });
+        }
+        closeForm();
+        //fonction qui gère le contenu des inputs
+        const errorDisplay = (tag, message, valid) => {
+            const container = document.querySelector("." + tag + "-formData");
+            const span = document.querySelector("." + tag + "-formData > span");
+            if (!valid) {
+                container.classList.add("error");
+                span.textContent = message;
+            } else {
+                container.classList.remove("error");
+                span.textContent = message;
+            }
+
+        };
+        //check prénom et nom
+        const firstLastChecker = (value, tag) => {
+            if (!value) {
+                errorDisplay(tag, "Le champ doit être rempli!");
+            } else if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+                errorDisplay(tag, "Veuillez entrer 2 caractères ou plus");
+
+            } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+                errorDisplay(tag, "Le pseudo ne doit pas contenir de caractères spéciaux");
+
+            } else {
+                errorDisplay(tag, "", true);
+                return true;
+            }
+        };
+        //check sur email
+        const emailChecker = (value, tag) => {
+            if (value == "") {
+                errorDisplay(tag, "Le champ doit être rempli!");
+
+            } else if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+                errorDisplay(tag, "Le mail n'est pas valide");
+
+            } else {
+                errorDisplay(tag, "", true);
+                return true;
+            }
+        };
+        //check texte (à faire...)
+        //listener sur prénom
+        //Récupération de l'id first dans le DOM
+        let firstInput = document.getElementById("first");
+
+        //Ajout du listener sur l'élément et contrôle:
+        firstInput.addEventListener('change', (e) => {
+            firstLastChecker(e.target.value, "first");
+        });
+        //listener sur nom
+        //Récupération de l'id last dans le DOM
+        let lastInput = document.getElementById("last");
+
+        //Ajout du listener sur l'élément et contrôle:
+        lastInput.addEventListener('change', (e) => {
+            firstLastChecker(e.target.value, "last");
+        });
+        //listener sur email
+        let emailInput = document.getElementById("email");
+
+        //Ajout du listener sur l'élément et contrôle:
+        emailInput.addEventListener('change', (e) => {
+            emailChecker(e.target.value, "email");
+        });
+        //listener sur texte (à faire)
+        let textInput = document.getElementById("improve");
+
+        //Ajout du listener sur l'élément et contrôle:
+        // textInput.addEventListener('change', (e) => {
+        //     console.log(e.target.value, "Message pour amélioration")
+        // });
+        //validation du formulaire
+        onlyForm.addEventListener('submit', (e) => {
+            //empêche l'envoi du formulaire
+            e.preventDefault();
+            //Récupération de la valeur des inputs au submit
+            firstValue = firstInput.value;
+            lastValue = lastInput.value;
+            emailValue = emailInput.value;
+            textValue = textInput.value;
+            // console.log("Teste envoi du formulaire");
+            // Envoi des valeurs de chaque champ (input) vers la fonction de test et validation
+            firstLastChecker(firstValue, "first");
+            firstLastChecker(lastValue, "last");
+            emailChecker(emailValue, "email");
+            //if= si c true envoi du formulaire, reset, vidage des variables.
+            if (firstLastChecker(firstValue, "first") &&
+                firstLastChecker(lastValue, "last") &&
+                emailChecker(emailValue, "email")
+            ) {
+                console.log("Prénom: ", firstValue);
+                console.log("Nom: ", lastValue);
+                console.log("Email: ", emailValue);
+                console.log("Message: ", textValue);
+
+                //Fermeture de la Modale
+                closeForm();
+                // Effacement des champs du formulaire
+                document.getElementById("form").reset();
+                firstValue = "";
+                lastValue = "";
+                emailValue = "";
+                textValue = "";
+            }
+
         });
     }
+    //Lancement du formulaire au clic
     form.addEventListener('click', e => {
         e.preventDefault();
-        console.log("Cliqué+++++++++++++++");
+        // console.log("Cliqué+++++++++++++++");
         displayForm(photogapherName, modale);
+        submitForm();
     });
 }
