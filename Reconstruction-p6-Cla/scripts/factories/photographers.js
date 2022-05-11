b3a70d15ba5dea49dbcabe85a356a8d0df542826
Claire_M <clairerodriguez79@yahoo.fr>
2022-05-11 15:27:23 +0200

    Balises sémantiques sur index.html

--- Reconstruction-p6-Cla/scripts/factories/photographers.js
+++ Reconstruction-p6-Cla/scripts/factories/photographers.js
@@ -11,25 +11,25 @@ class Photographer {
 
     displayPhotographer() {
         return `
-        <div class="photographer">
-        <a href="./photographer.html?${this.id}">
-            <div class="card">
-                <div class="container-img">
-                    <img class="container-img__img" src="assets/photographers/Photographers ID Photos/${this.portrait}" />
+        <article class="photographer">
+            <a href="./photographer.html?${this.id}">
+                <div class="card">
+                    <div class="container-img">
+                        <img class="container-img__img" src="assets/photographers/Photographers ID Photos/${this.portrait}" alt="Portrait de ${this.name}" />
+                    </div>
+                    <h2 class="card__photographer-name">${this.name}</h2>
                 </div>
-                <h2 class="card__photographer-name">${this.name}</h2>
-            </div>
-        </a>
-        <p>${this.city}</p>
-        <p>${this.tagline}</p>
-        <p>${this.price}</p>
-    </div>`
+            </a>
+            <p>${this.city}</p>
+            <p>${this.tagline}</p>
+            <p>${this.price}</p>
+        </article>`
     }
 
-    //Fonction qui gère l'affichage du template du header
+    //Fonction qui gère l'affichage du header dans la page photographer.html
     displayHeader() {
         return `        
-        <div class="photograph-header__title">
+            <div class="photograph-header__title">
                 <h1>${this.name}</h1>
                 <p>${this.city}, ${this.country}</p>
                 <p>${this.tagline}</p>
@@ -40,7 +40,7 @@ class Photographer {
             <div id="form-contact" style="display:none">
             </div>
             <div class="photograph-header__container-img">
-                <img class="photograph-header__container-img__img" src="assets/photographers/Photographers ID Photos/${this.portrait}" />
+                <img class="photograph-header__container-img__img" src="assets/photographers/Photographers ID Photos/${this.portrait}" alt="portrait de${this.name} " />
             </div>`
     }
     displayPrice() {
